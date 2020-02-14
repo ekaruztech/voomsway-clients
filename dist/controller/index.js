266ca7836adea5a32efdb87f8cf0b22f55545b6d
nondefyde <nondefyde@gmail.com>
2020-02-14 14:27:23 +0100

    integrattion

--- dist/controller/index.js
+++ dist/controller/index.js
@@ -19,51 +19,11 @@ var AppController = {
       title: 'Efex Transport'
     });
   },
-  contact: function () {
-    var _contact = (0, _asyncToGenerator2["default"])(
-    /*#__PURE__*/
-    _regenerator["default"].mark(function _callee(req, res, next) {
-      var _ref, _ref$data, location, contact_info;
-
-      return _regenerator["default"].wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              _context.next = 2;
-              return _request["default"].getAccount(process.env.VOOMSWAY_API_KEY);
-
-            case 2:
-              _ref = _context.sent;
-              _ref$data = _ref.data;
-              location = _ref$data.location;
-              contact_info = _ref$data.contact_info;
-
-              if (location || contact_info) {
-                res.render('contact', {
-                  title: 'Efex Transport',
-                  location: location,
-                  contact_info: contact_info
-                });
-              } else {
-                res.render('contact', {
-                  title: 'Efex Transport'
-                });
-              }
-
-            case 7:
-            case "end":
-              return _context.stop();
-          }
-        }
-      }, _callee);
-    }));
-
-    function contact(_x, _x2, _x3) {
-      return _contact.apply(this, arguments);
-    }
-
-    return contact;
-  }(),
+  contact: function contact(req, res, next) {
+    res.render('contact', {
+      title: 'Efex Transport'
+    });
+  },
   about: function about(req, res, next) {
     return res.render('about', {
       title: 'Efex Transport'
@@ -82,19 +42,19 @@ var AppController = {
   entry: function () {
     var _entry = (0, _asyncToGenerator2["default"])(
     /*#__PURE__*/
-    _regenerator["default"].mark(function _callee2(req, res, next) {
-      var _ref2, data;
+    _regenerator["default"].mark(function _callee(req, res, next) {
+      var _ref, data;
 
-      return _regenerator["default"].wrap(function _callee2$(_context2) {
+      return _regenerator["default"].wrap(function _callee$(_context) {
         while (1) {
-          switch (_context2.prev = _context2.next) {
+          switch (_context.prev = _context.next) {
             case 0:
-              _context2.next = 2;
+              _context.next = 2;
               return _request["default"].getAccount(process.env.VOOMSWAY_CLIENT_KEY);
 
             case 2:
-              _ref2 = _context2.sent;
-              data = _ref2.data;
+              _ref = _context.sent;
+              data = _ref.data;
               console.log('data :::: ', data);
               res.render('trips', {
                 host: process.env.HOST,
@@ -105,13 +65,13 @@ var AppController = {
 
             case 6:
             case "end":
-              return _context2.stop();
+              return _context.stop();
           }
         }
-      }, _callee2);
+      }, _callee);
     }));
 
-    function entry(_x4, _x5, _x6) {
+    function entry(_x, _x2, _x3) {
       return _entry.apply(this, arguments);
     }
 
@@ -120,23 +80,23 @@ var AppController = {
   terminals: function () {
     var _terminals = (0, _asyncToGenerator2["default"])(
     /*#__PURE__*/
-    _regenerator["default"].mark(function _callee3(req, res, next) {
-      var page, _ref3, _meta, data;
+    _regenerator["default"].mark(function _callee2(req, res, next) {
+      var page, _ref2, _meta, data;
 
-      return _regenerator["default"].wrap(function _callee3$(_context3) {
+      return _regenerator["default"].wrap(function _callee2$(_context2) {
         while (1) {
-          switch (_context3.prev = _context3.next) {
+          switch (_context2.prev = _context2.next) {
             case 0:
               page = req.query.page || 1;
-              _context3.next = 3;
+              _context2.next = 3;
               return _request["default"].getTerminals({
                 page: page
               });
 
             case 3:
-              _ref3 = _context3.sent;
-              _meta = _ref3._meta;
-              data = _ref3.data;
+              _ref2 = _context2.sent;
+              _meta = _ref2._meta;
+              data = _ref2.data;
 
               if (data) {
                 res.render('terminals', {
@@ -154,13 +114,13 @@ var AppController = {
 
             case 7:
             case "end":
-              return _context3.stop();
+              return _context2.stop();
           }
         }
-      }, _callee3);
+      }, _callee2);
     }));
 
-    function terminals(_x7, _x8, _x9) {
+    function terminals(_x4, _x5, _x6) {
       return _terminals.apply(this, arguments);
     }
 
