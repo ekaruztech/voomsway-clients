ef8281f99593a8fd7ed8d73e52e94cdd6e32f612
kheengz <kheengz@gmail.com>
2020-06-23 22:14:16 +0100

    fixes broken url

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import { AppController } from '../controller';
 const router = Router();
 
 router.get('/', AppController.index);
-router.get('/vway/*', AppController.entry);
+// router.get('/vway/*', AppController.entry);
 router.get('/terminals', AppController.terminals);
 router.get('/about', AppController.about);
 router.get('/contact', AppController.contact);
